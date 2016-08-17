9a67c84f2b2e137a67d16338470459bd3aaabbb4
SneakyTactician <sneakytactician@outlook.com>
2016-08-17 12:59:08 -0500

    Adding some classes

---
+++ SomeCardGames/SomeCardGames/Drawing/Graphics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeCardGames.Drawing
+{
+    /// <summary>
+    /// This class does the drawing for this game.
+    /// </summary>
+    public static class Graphics
+    {
+
+    }
+}
