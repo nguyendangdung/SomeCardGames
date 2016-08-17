97f45facdc9c246ea71ee6844550763609b93f2b
SneakyTactician <sneakytactician@outlook.com>
2016-08-17 14:47:50 -0500

    Added a nuget package that will enable me to interact with github

---
+++ SomeCardGames/SomeCardGames/Error/ErrorReporter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeCardGames.Error
+{
+    public static class ErrorReporter
+    {
+        public static void Report(Exception ex)
+        {
+            WebRequest request = WebRequest.Create("https://api.github.com/repos/SneakyTactician/SomeCardGames/issues ");
+            request.Method = "POST";
+            string postData = "{'title':'exception occured!', 'body':'{0}','assignee': 'SneakyTactician'}";
+            byte[] byteArray = Encoding.UTF8.GetBytes(/*string.Format(postData, ex)*/"a");
+            request.ContentLength = byteArray.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+            WebResponse response = request.GetResponse();
+         }
+    }
+}
--- SomeCardGames/SomeCardGames/Form1.cs
+++ SomeCardGames/SomeCardGames/Form1.cs
@@ -12,14 +12,23 @@ namespace SomeCardGames
 {
     public partial class Form1 : Form
     {
+        Main main = new Main();
+        /// <summary>
+        /// The inital call to this program.
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// This is called when the form loads.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            main.CaptureTheForm(1);
         }
     }
 }
