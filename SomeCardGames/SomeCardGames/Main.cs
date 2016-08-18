6ea33e7a9a8c70477a2638cbd6810eca56c6a0ca
SneakyTactician <sneakytactician@outlook.com>
2016-08-18 13:44:57 -0500

    Window now fullscreens itself

--- SomeCardGames/SomeCardGames/Main.cs
+++ SomeCardGames/SomeCardGames/Main.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 using SomeCardGames.Error;
+using SomeCardGames.Utility;
 
 namespace SomeCardGames
 {
@@ -21,6 +24,8 @@ namespace SomeCardGames
         public void Startup(object Form)
         {
             this.CaptureTheForm(Form);
+            this.GetScreenSize();
+            this.SetupForm();
         }
 
         /// <summary>
@@ -33,9 +38,35 @@ namespace SomeCardGames
             {
                 TheForm = (Form1)Form;
             }
-            catch (InvalidCastException InvalidCast)
+            catch (Exception e)
             {
-                ErrorReporter.Report(InvalidCast);
+                ErrorReporter.Report(e);
+            }
+        }
+
+        /// <summary>
+        /// Sets up the form.
+        /// </summary>
+        public void SetupForm()
+        {
+            TheForm.TopMost = true;
+            TheForm.FormBorderStyle = FormBorderStyle.None;
+            TheForm.WindowState = FormWindowState.Maximized;
+        }
+
+        /// <summary>
+        /// Gets the size of the moniter.
+        /// </summary>
+        public void GetScreenSize()
+        {
+            try
+            {
+                VariableStorage.ScreenWidth = Screen.FromControl(TheForm).WorkingArea.Width;
+                VariableStorage.ScreenHeight = Screen.FromControl(TheForm).WorkingArea.Height;
+            }
+            catch(Exception e)
+            {
+                ErrorReporter.Report(e);
             }
         }
     }
---
+++ SomeCardGames/SomeCardGames/Utility/VariableStorage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeCardGames.Utility
+{
+    /// <summary>
+    /// A class used to store universal variables.
+    /// </summary>
+    public static class VariableStorage
+    {
+        /// <summary>
+        /// Represents how wide the screen is.
+        /// </summary>
+        public static int ScreenWidth = 0;
+
+        /// <summary>
+        /// Represents how tall the screen is.
+        /// </summary>
+        public static int ScreenHeight = 0;
+    }
+}
