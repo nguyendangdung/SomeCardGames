60c366a090c78e718900287a42edae5db7b79ba4
SneakyTactician <sneakytactician@outlook.com>
2016-08-22 13:57:50 -0500

    Removing class containing token from git

---
+++ SomeCardGames/SomeCardGames/Base/Deck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeCardGames.Base
+{
+    /// <summary>
+    /// Represents a deck of cards in the game.
+    /// </summary>
+    public class Deck
+    {
+
+    }
+}
