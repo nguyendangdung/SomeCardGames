b359148c1f4be1109799c820b3b9907415018dd7
SneakyTactician <sneakytactician@outlook.com>
2016-08-22 13:17:03 -0500

    Creating base classes that are needed for all card games.

---
+++ SomeCardGames/SomeCardGames/Base/Card.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// This namespace is used to store various things that every card game uses.
+/// </summary>
+namespace SomeCardGames.Base
+{
+    /// <summary>
+    /// Used to represent a card in the game.
+    /// </summary>
+    public class Card
+    {
+
+        /// <summary>
+        /// The constructor for the Card class.
+        /// </summary>
+        /// <param name="TheCard"></param>
+        public Card(int TheCard)
+        {
+
+        }
+
+        /// <summary>
+        /// Used to tell what card this card is.
+        /// </summary>
+        int WhatCard;
+
+
+        /// <summary>
+        /// Used to compare all of the different cards in a deck of cards.
+        /// </summary>
+        public enum Cards
+        {
+
+        }
+    }
+}
