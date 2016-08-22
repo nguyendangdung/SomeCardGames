841e27176a289a6fc7bd228d243cdbddcf685d7b
SneakyTactician <sneakytactician@outlook.com>
2016-08-22 14:51:19 -0500

    Got github to ignore my error storage file. Documented some stuff.

--- SomeCardGames/SomeCardGames/Error/ErrorStorage.cs
+++ SomeCardGames/SomeCardGames/Error/ErrorStorage.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace SomeCardGames.Error
 {
+    /// <summary>
+    /// This class is used to store error messages.
+    /// </summary>
     public static class ErrorStorage
     {
         public static string CardNotFoundException = "A caller attempted to create a card that doesn't exist";
