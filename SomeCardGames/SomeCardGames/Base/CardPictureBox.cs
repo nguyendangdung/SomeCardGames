6fe81f7b247dccc2f858f5bf27e0f9281e6f67b6
SneakyTactician <sneakytactician@outlook.com>
2016-08-23 17:05:45 -0500

    Writing more framework stuff.

---
+++ SomeCardGames/SomeCardGames/Base/CardPictureBox.cs
@@ -0,0 +1,53 @@
+﻿using SomeCardGames.Error;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeCardGames.Base
+{
+    /// <summary>
+    /// This is used to render a card, and to interpret different input events.
+    /// </summary>
+    public class CardPictureBox
+    {
+        /// <summary>
+        /// The image of this picture box.
+        /// </summary>
+        private Bitmap Image;
+
+        /// <summary>
+        /// The constructor for the CardPictureBox class.
+        /// </summary>
+        public CardPictureBox(int Card)
+        {
+            try
+            {
+
+            }
+            catch (Exception TheException)
+            {
+                ErrorReporter.Report(TheException);
+            }
+        }
+        /// <summary>
+        /// Used if you want to draw something besides a card with this.
+        /// </summary>
+        /// <param name="image"></param>
+        public CardPictureBox(Bitmap image)
+        {
+            try
+            {
+                Image = image;
+            }
+            catch (Exception TheException)
+            {
+                ErrorReporter.Report(TheException);
+            }
+        }
+    }
+}
---
+++ SomeCardGames/SomeCardGames/Utility/Util.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeCardGames.Utility
+{
+    /// <summary>
+    /// Holds some various random utility functions.
+    /// </summary>
+    public static class Util
+    {
+        /// <summary>
+        /// Loads a bitmap from the resources file.
+        /// </summary>
+        /// <param name="FileName"></param>
+        /// <returns></returns>
+        public static Bitmap LoadResource(string FileName)
+        {
+            System.Reflection.Assembly asm = System.Reflection.Assembly.GetExecutingAssembly();
+            string resourceName = asm.GetName().Name + ".Properties.Resources";
+            var rm = new System.Resources.ResourceManager(resourceName, asm);
+            return (Bitmap)rm.GetObject(FileName);
+        }
+    }
+}
