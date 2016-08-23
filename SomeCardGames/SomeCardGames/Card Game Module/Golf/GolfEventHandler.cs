c8f0ebc9b2df67f7517f94f3353ef3334b5cab59
SneakyTactician <sneakytactician@outlook.com>
2016-08-23 16:24:15 -0500

    Setting up the shell for the golf game.

---
+++ SomeCardGames/SomeCardGames/Card Game Module/Golf/GolfEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeCardGames.Card_Game_Module.Golf
+{
+    /// <summary>
+    /// This is used to recieve and process mouse and keyboard events.
+    /// </summary>
+    public class GolfEventHandler
+    {
+
+    }
+}
---
+++ SomeCardGames/SomeCardGames/Drawing/Drawers/GolfDrawer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeCardGames.Drawing.Drawers
+{
+    /// <summary>
+    /// Draws the graphics for the Golf card game.
+    /// </summary>
+    public class GolfDrawer : IDrawer
+    {
+        /// <summary>
+        /// Called by the paint event on the form. Draws the current status of the golf card game.
+        /// </summary>
+        /// <param name="G"></param>
+        public void Draw(Graphics G)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
