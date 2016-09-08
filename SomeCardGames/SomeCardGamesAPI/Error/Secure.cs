6c5db0eb5d71683dd24fb479dc23a185229f3db1
SneakyTactician <sneakytactician@outlook.com>
2016-09-08 17:20:57 -0500

    Moved more files over.

---
+++ SomeCardGames/SomeCardGamesAPI/Error/Secure.cs
@@ -0,0 +1,17 @@
+﻿namespace SomeCardGames.Error
+{
+    /// <summary>
+    /// The stuff in this class isn't just given out.
+    /// </summary>
+    public static class Secure
+    {
+        /// <summary>
+        /// Returns a token for my github page.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetGitToken()
+        {
+            return "59bb6bd2eacc3fe369f794164304906cac803f64";
+        }
+    }
+}
