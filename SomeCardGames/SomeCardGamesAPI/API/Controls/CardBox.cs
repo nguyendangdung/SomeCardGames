97811056295aa72138f96fa1ca2b757bfe34d529
SneakyTactician <sneakytactician@outlook.com>
2016-09-12 16:41:19 -0500

    Built most of the CardBox class.

---
+++ SomeCardGames/SomeCardGamesAPI/API/Controls/CardBox.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SomeCardGamesAPI.Error;
+using SomeCardGames.Error;
+
+namespace SomeCardGamesAPI.API.Controls
+{
+    /// <summary>
+    /// Basically a PictureBox.
+    /// </summary>
+    public class CardBox
+    {
+        /// <summary>
+        /// Holds the image this CardBox draws.
+        /// </summary>
+        public Bitmap Image;
+
+        /// <summary>
+        /// If true, the draw method will try to draw the Image.
+        /// </summary>
+        public bool Visible = true;
+
+        /// <summary>
+        /// How wide and tall this is.
+        /// </summary>
+        public Size Size;
+
+        /// <summary>
+        /// The top left corner of this.
+        /// </summary>
+        public Point Location;
+
+        /// <summary>
+        /// The constructor for the CardBox class.
+        /// </summary>
+        public CardBox()
+        {
+
+        }
+        /// <summary>
+        /// The constructor for the CardBox class.
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="size"></param>
+        /// <param name="TopLeft"></param>
+        public CardBox(Bitmap image, Size size, Point TopLeft)
+        {
+            try
+            {
+                this.Image = image;
+                size = Size;
+                Location = TopLeft;
+            }
+            catch (Exception TheException)
+            {
+                ErrorReporter.Report(TheException);
+            }
+        }
+
+        /// <summary>
+        /// Draws.
+        /// </summary>
+        /// <param name="g">The graphics object everyone is passing around.</param>
+        public void Draw(Graphics g)
+        {
+            try
+            {
+                if (Visible)
+                {
+                    g.DrawImage(this.Image, this.Location.X, this.Location.Y, this.Size.Width, this.Size.Height);
+                }
+            }
+            catch (Exception TheException)
+            {
+                ErrorReporter.Report(TheException);
+            }
+        }
+    }
+}
