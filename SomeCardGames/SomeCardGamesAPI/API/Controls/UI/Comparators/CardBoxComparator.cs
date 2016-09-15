6965689b5452f3234f504bd70317fa02a4ad58d4
SneakyTactician <sneakytactician@outlook.com>
2016-09-15 16:18:18 -0500

    working on the UI class.

---
+++ SomeCardGames/SomeCardGamesAPI/API/Controls/UI/Comparators/CardBoxComparator.cs
@@ -0,0 +1,34 @@
+﻿using SomeCardGamesAPI.Error;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeCardGamesAPI.API.Controls.UI.Comparators
+{
+    /// <summary>
+    /// This class is used to compare the draw order of CardBox, used by lists.
+    /// </summary>
+    public class CardBoxComparator : IComparer<CardBox>
+    {
+        /// <summary>
+        /// Actually compares two CardBox objects.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public int Compare(CardBox x, CardBox y)
+        {
+            try
+            {
+                return x.DrawOrder - y.DrawOrder;
+            }
+            catch (Exception TheException)
+            {
+                ErrorReporter.Report(TheException);
+                return 0;
+            }
+        }
+    }
+}
---
+++ SomeCardGames/SomeCardGamesAPI/API/Controls/UI/UI.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using SomeCardGamesAPI.API.Controls.UI;
+using SomeCardGamesAPI.API.Controls.UI.Comparators;
+using SomeCardGamesAPI.Error;
+
+namespace SomeCardGamesAPI.API.Controls.UI
+{
+    /// <summary>
+    /// A class that creates the basis of a UI concept.
+    /// </summary>
+    public class UI
+    {
+        /// <summary>
+        /// The form the controls we have use.
+        /// </summary>
+        public Form form;
+
+        /// <summary>
+        /// All of the card boxes that we have here.
+        /// </summary>
+        public List<CardBox> Controls;
+        
+        /// <summary>
+        /// Used to order objects into the Controls list properly.
+        /// </summary>
+        public CardBoxComparator Comparator = new CardBoxComparator();
+
+        bool Visible = true;
+
+        /// <summary>
+        /// The constructor for the UI class.
+        /// </summary>
+        /// <param name="TheForm"></param>
+        /// <param name="controls"></param>
+        /// <param name="Visibility"></param>
+        public UI(object TheForm, CardBox[] controls, bool Visibility)
+        {
+            try
+            {
+                this.form = (Form)TheForm;
+                this.Visible = Visibility;
+
+                foreach (CardBox item in controls)
+                {
+                    this.AddCardBox(item);
+                }
+            }
+            catch (Exception TheException)
+            {
+                ErrorReporter.Report(TheException);
+            }
+        }
+
+        /// <summary>
+        /// Adds said control into the controls list.
+        /// </summary>
+        /// <param name="control"></param>
+        public void AddCardBox(CardBox control)
+        {
+            try
+            {
+                //Supposedly, this should insert objects into the correct order.
+                int index = this.Controls.BinarySearch(control, this.Comparator);
+                if (index < 0) index = ~index;
+                this.Controls.Insert(index, control);
+            }
+            catch (Exception TheException)
+            {
+                ErrorReporter.Report(TheException);
+            }
+        }
+    }
+}
