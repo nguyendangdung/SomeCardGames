7223bd23fc3ea6ca4423b92b2804c6fe37d5ea2a
SneakyTactician <sneakytactician@outlook.com>
2016-09-25 14:03:20 -0500

    Created empty unit tests for the SomeCardGames project.

---
+++ SomeCardGames/SomeCardGamesTests1/MainTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SomeCardGames;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeCardGames.Tests
+{
+    [TestClass()]
+    public class MainTests
+    {
+        [TestMethod()]
+        public void StartupTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ShutdownTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void StartNewCardGameTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
