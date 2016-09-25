b9ab165b03b060adadd2bcd420fe1b9122cd13b0
SneakyTactician <sneakytactician@outlook.com>
2016-09-25 15:49:03 -0500

    Added a test resources to the current test lib

--- SomeCardGames/SomeCardGamesAPITests/API/Controls/UI/UITests.cs
+++ SomeCardGames/SomeCardGamesAPITests/API/Controls/UI/UITests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SomeCardGamesAPI.API.Controls.UI.Tests
 {
@@ -16,8 +17,10 @@ namespace SomeCardGamesAPI.API.Controls.UI.Tests
         {
             try
             {
+                //Normal Path
                 UI a;
                 a = new UI();
+                //
             }
             catch (Exception e)
             {
@@ -26,15 +29,45 @@ namespace SomeCardGamesAPI.API.Controls.UI.Tests
         }
 
         [TestMethod()]
-        public void ConstructMeTest()
+        public void ConstructMeNormalTest()
         {
             try
             {
-
+                Form a = this.GenerateForm(true);
+                CardBox b = new CardBox()
             }
             catch (Exception e)
             {
                 Assert.Fail(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// If normal = true, we generate a non null form. Otherwise, the form is null;
+        /// </summary>
+        /// <param name="Normal"></param>
+        private Form GenerateForm(bool Normal)
+        {
+            try
+            {
+                if (Normal)
+                {
+                    Form a;
+
+                    a = new Form();
+                    a.Text = "Test Title";
+
+                    return a;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+                return null;
             }
         }
 
