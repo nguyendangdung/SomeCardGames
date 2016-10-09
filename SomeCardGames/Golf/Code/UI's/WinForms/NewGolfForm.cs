e62a78934ee38dde4f259bfcbcb45decb5d0b43c
SneakyTactician <sneakytactician@outlook.com>
2016-10-08 23:06:54 -0500

    Began to work on the newgolfform.

---
+++ SomeCardGames/Golf/Code/UI's/WinForms/NewGolfForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Golf.Code.UI_s.WinForms
+{
+    /// <summary>
+    /// This form is called when creating a new golf game.
+    /// </summary>
+    public class NewGolfForm
+    {
+    }
+}
