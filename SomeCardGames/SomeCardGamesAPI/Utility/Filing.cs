c45be6f35b63b194116a236b4e88534e7be64b42
SneakyTactician <sneakytactician@outlook.com>
2016-10-17 16:58:39 -0500

    Working on a file path class.

---
+++ SomeCardGames/SomeCardGamesAPI/Utility/Filing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeCardGamesAPI.Utility
+{
+	/// <summary>
+	/// This class is used to manage all of the folders and file paths for this game, extensions, and it's modules.
+	/// </summary>
+	public static class Filing
+	{
+
+	}
+}
